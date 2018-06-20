ff1e5226a9f61460fb020dca09d4493fc6440eef
Alex Sleat <alexsleat@gmail.com>
2018-06-20 16:22:21 +0100

    removed specifying location

--- hearts_setup/install/clone_all_repos.sh
+++ hearts_setup/install/clone_all_repos.sh
@@ -1,2 +1,2 @@
 ORGANIZATION=HeartsBRL
-for i in `curl -s https://api.github.com/orgs/$ORGANIZATION/repos?per_page=200 |grep html_url|awk 'NR%2 == 0'|cut -d ':'  -f 2-3|tr -d '",'`; do  git clone $i.git ~/workspaces/hearts_erl/src/;  done
+for i in `curl -s https://api.github.com/orgs/$ORGANIZATION/repos?per_page=200 |grep html_url|awk 'NR%2 == 0'|cut -d ':'  -f 2-3|tr -d '",'`; do  git clone $i.git;  done
