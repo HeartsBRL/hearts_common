6bdd1f22efa6e63a49ee9d584fb05359711706ad
Richard Wakeling-Gentle <32925129+Numjerlunker@users.noreply.github.com>
2018-09-27 20:21:18 +0100

    added hearts_bagging - package for launching rosbag for each of the tasks
    see the readme file for more information
    the directory in the .sh files needs to be changed to match directory on tiago

---
+++ hearts_bagging/launch/bagging_TBM2.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# changeable variables.
+filepath="/home/rwakelingg/workspaces/hearts_erl/src/hearts_bagging/bags"
+category="TBM2"
+teamname="HEARTS"
+now=$(date +"%Y%m%d%H%M")
+
+
+#see README for more information on these outputs.
+# the '\' makes the file ignore the new line - for better formatting and readability. 
+rosbag record \
+-O ${filepath}/${category}_${now}_${teamname} \
+/erlc/robot_pose \
+/erlc/marker_pose \
+/erlc/trajectory \
+/erlc/DEVICE/image \
+/erlc/DEVICE/camera_info \
+/erlc/depth_ID/pointcloud \
+/erlc/scan_ID \
+tf \
+/ERLSCR/command \
+/ERLSCR/visitor \
+/ERLSCR/audio \
+/ERLSCR/notification \
+__name:=TBM2bagging
+
+
+
+
+
